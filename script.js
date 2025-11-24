ca85349512633edda524b88fa71afc3649454667
Janbourah <ajanbourah@gmail.com>
2025-11-24 20:12:53 +0300

    javascript updates

--- script.js
+++ script.js
@@ -29,6 +29,9 @@ discardBtn.addEventListener("click" , () => {
   const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id)
 
   const taskObj = {
-    id: titleInput.value.toLowerCase(),
+    id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+    title: titleInput.value,
+    date: dateInput.value,
+    description: descriptionInput.value
   }
 console.log(taskObj)
